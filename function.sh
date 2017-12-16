4f8b1eefb724095c1dc3d4c4da82d890435d2801
Max <sherlockmax226@gmail.com>
2017-12-16 22:46:06 +0800

    change main bash's name

--- function.sh
+++ function.sh
@@ -1,25 +1,25 @@
 function callHelp {
-    echo "指令: main.sh {命令} [參數]"
+    echo "指令: nsc.sh {命令} [參數]"
     cat <<EOF
     
 start...........啟動nsq container ship
-                指令: main.sh start
+                指令: nsc.sh start
 stop............停止nsq container ship
-                指令: main.sh stop
+                指令: nsc.sh stop
 restart.........重新啟動nsq container ship
-                指令: main.sh restart
+                指令: nsc.sh restart
 create topic....建立新主題(topic)
-                指令: main.sh create topic [:主題名稱]
+                指令: nsc.sh create topic [:主題名稱]
 create channel..於指定的主題(topic)中建立新頻道(channel)
-                指令: main.sh create topic [:主題名稱] [:頻道名稱]
+                指令: nsc.sh create topic [:主題名稱] [:頻道名稱]
 send............傳送訊息到指定的主題(topic)
-                指令: main.sh send [:主題名稱] [:訊息]
+                指令: nsc.sh send [:主題名稱] [:訊息]
 listen..........監聽指定的主題(topic)頻道(channel)的訊息，若只給主題(topic)則會自己建立一個新的頻道並監聽
-                指令: main.sh listen [:主題名稱] [頻道名稱]
+                指令: nsc.sh listen [:主題名稱] [頻道名稱]
 self_update.....nsq container ship的自我版本更新
-                指令: main.sh self_update [:主題名稱] [頻道名稱]
+                指令: nsc.sh self_update [:主題名稱] [頻道名稱]
 help............使用說明
-                指令: main.sh help
+                指令: nsc.sh help
 
 EOF
     exit 0
--- readme.md
+++ readme.md
@@ -18,31 +18,31 @@
 ### 1. start container ship
 
 ```bash
-main.sh start
+nsc.sh start
 ```
 
 ### 2. create topic
 
 ```bash
-main.sh create topic "topic1"
+nsc.sh create topic "topic1"
 ```
 
 ### 3. create channel
 
 ```bash
-main.sh create channel "topic1" "channel1"
+nsc.sh create channel "topic1" "channel1"
 ```
 
 ### 4. open a new terminal, and create listener to listen the channel
 
 ```bash
-main.sh listen "topic1" "channel1"
+nsc.sh listen "topic1" "channel1"
 ```
 
 ### 5. send message to topic, and listener will recive the message
 
 ```bash
-main.sh send "topic1" "this is a testing message."
+nsc.sh send "topic1" "this is a testing message."
 
 # send message by curl : 
 curl -d "message" "http://localhost:4151/pub?topic=topic1"
@@ -53,13 +53,13 @@ curl -d "message" "http://localhost:4151/pub?topic=topic1"
 ### 1. show help
 
 ```bash
-main.sh help
+nsc.sh help
 ```
 
 ### 2. close or restart all container
 
 ```bash
-main.sh stop
+nsc.sh stop
 # or
-main.sh restart
+nsc.sh restart
 ```
